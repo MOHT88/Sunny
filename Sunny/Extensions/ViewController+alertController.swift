47466ea2cb1793540b35453a4978829b9a1309c8
Sergey Ivanov <regspam88@ya.ru>
2022-10-11 11:49:45 +0300

    Initial Commit

---
+++ Sunny/Extensions/ViewController+alertController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController+alertController.swift
+//  Sunny
+//
+//  Created by Ivan Akulov on 25/02/2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+
+import UIKit
+
+extension ViewController {
+    func presentSearchAlertController(withTitle title: String?, message: String?, style: UIAlertController.Style) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        alert.addTextField { tf in
+            let cities = ["San Francisco", "Moscow", "New York", "Stambul", "Viena"]
+            tf.placeholder = cities.randomElement()
+        }
+        let search = UIAlertAction(title: "Search", style: .default) { action in
+            let textField = alert.textFields?.first
+            guard let cityName = textField?.text else { return }
+            if cityName != "" {
+                print("search info for the \(cityName)")
+            }
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        alert.addAction(search)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+}
---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Сергей Иванов on 11.10.2022.
+//  Copyright © 2022 Ivan Akulov. All rights reserved.
+//
+
+import Foundation
+
+
+let apiKey = "fa2b0882f1c236e1a1283e3197430bf5"
