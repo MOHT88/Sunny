92bc6012ade9b67bd08392bfdbaf71f4eed724d6
Sergey Ivanov <regspam88@ya.ru>
2022-10-11 18:16:36 +0300

    Update labels

--- Sunny/Models/NetworkWeatherManager.swift
+++ Sunny/Models/NetworkWeatherManager.swift
@@ -8,21 +8,26 @@
 
 import Foundation
 
-struct NetworkWeatherManager {
+class NetworkWeatherManager {
+    
+    var onCompletion: ((CurrentWeather) -> Void)?
+    
     func fetchCurrentWeather(forCity city: String) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=\(apiKey)&units=metric"
         guard let url = URL(string: urlString) else { return }
         let session = URLSession(configuration: .default)
         let task = session.dataTask(with: url) { data, response, error in
             if let data = data {
-           let currentWeather = parseJSON(withDate: data)
+                if let currentWeather = self.parseJSON(withData: data) {
+                    self.onCompletion?(currentWeather)
+                }
             }
         }
         task.resume()
     }
     
     
-    func parseJSON(withDate data: Data) -> CurrentWeather? {
+    func parseJSON(withData data: Data) -> CurrentWeather? {
         let decoder = JSONDecoder()
         do {
              let currentWeatherData = try decoder.decode(CurrentWeatherData.self, from: data)
